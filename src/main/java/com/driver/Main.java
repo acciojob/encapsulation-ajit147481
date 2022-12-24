dc48e08fe06e53d22e926e8666ebeee767d3f3a8
ajit147481 <rautajit1234@gmail.com>
2022-12-24 16:53:05 +0530

    a

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj=new RWOnly();
+    //Cannot resolve symbol 'setName'
+    //obj.setName("ajit");
+    obj.setName("ajit");
+    System.out.println(obj.getName());
+  }
 }
